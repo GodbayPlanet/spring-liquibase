a609ee4db18b399636a5dd70a971e69480dabf71
Nemanja <nemanja.vasic@codecentric.de>
2019-05-17 16:54:31 +0200

    Change description in create-dev-db script

--- docker/create-dev-db.sh
+++ docker/create-dev-db.sh
@@ -3,7 +3,7 @@
 echo Re-creating and starting MySQL container...
 echo See file 'docker-compose-create-dev-db.yml' for details
 
-# Remove existing DB container and volumes
+# Stop and Remove existing DB container and volumes, then create and start container
 docker-compose -f docker-compose-create-dev-db.yml down -v
 docker-compose -f docker-compose-create-dev-db.yml up -d
 
