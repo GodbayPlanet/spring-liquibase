447878ed14130474f8b171adf87d7d2ffac4aa88
Nemanja <nemanja.vasic@codecentric.de>
2019-06-01 12:55:35 +0200

    Add book table and insert data using sql scripts

---
+++ src/main/resources/db/changelog/sql-scripts/add-book-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE book
+(
+    id         int          not null,
+    book_title varchar(255) not null,
+    author     varchar(255) not null,
+    pages      int          not null
+);
---
+++ src/main/resources/db/changelog/sql-scripts/add-books-to-book-table.sql
@@ -0,0 +1,5 @@
+INSERT INTO book (id, book_title, author, pages) VALUES (1, 'Java Introduction', 'Jovan Jovandovski', 356);
+INSERT INTO book (id, book_title, author, pages) VALUES (2, 'JavaScript Good Parts', 'Perica Peric', 786);
+INSERT INTO book (id, book_title, author, pages) VALUES (3, 'Angular In Action', 'Mladen Mladenkovic', 987);
+INSERT INTO book (id, book_title, author, pages) VALUES (4, 'React In Action', 'Jovanka Jovankic', 876);
+INSERT INTO book (id, book_title, author, pages) VALUES (4, 'Vue In Action', 'Jelena Jelendic', 345);
