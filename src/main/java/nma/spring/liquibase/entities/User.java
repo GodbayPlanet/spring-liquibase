2c523bdc7e8b64c234f0aa294d48200013d5f909
Nemanja <nemanja.vasic@codecentric.de>
2019-06-01 12:11:20 +0200

    Add lombok annotations for creating entity default and all argument constructors

--- src/main/java/nma/spring/liquibase/entities/User.java
+++ src/main/java/nma/spring/liquibase/entities/User.java
@@ -7,12 +7,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
